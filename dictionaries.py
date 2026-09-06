3da9ba272a98654a846fe00019db8360b03a6c34
Vinay Kumar Bethu <bethuvinaykumar@gmail.com>
2026-09-06 22:41:58 +0530

    Add Python dictionaries examples

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+student = {
+    "name": "Vinay",
+    "age": 21,
+    "branch": "AIML"
+}
+
+print(student)
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+
+student["age"] = 22
+
+print("Updated age:", student["age"])
+
+student["college"] = "Vahini Institute of Science and Technology"
+
+print(student)
